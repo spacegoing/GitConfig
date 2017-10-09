9eb89f8e8f40781f030718b2971d2bf3a795b106
spacegoing <spacegoing@gmail.com>
2017-10-09 13:51:03 +0800

    [New] sorg layer

---
+++ spacemacs/layers/sorg/packages.el
@@ -0,0 +1,22 @@
+;;; packages.el --- sorg layer packages file for Spacemacs.
+;;
+;; Copyright (c) 2012-2017 Sylvain Benner & Contributors
+;;
+;; Author: spacegoing <spacegoing@LCs-MacBook-Pro.local>
+;; URL: https://github.com/syl20bnr/spacemacs
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; License: GPLv3
+
+(defconst sorg-packages '((org-mode local)))
+
+(defun sdefaults/post-init-helm ()
+  (use-package org-mode
+    :defer t
+    :init (progn
+            (setq org-startup-indented t)
+            (setq org-agenda-files '("/Users/spacegoing/macCodeLab-MBP2015/MyPrivate/orgagenda")))))
+
+
+;;; packages.el ends here
