f9a17800daea542ca2491bd891d86cb3ba998f5b
spacegoing <spacegoing@gmail.com>
2022-10-05 12:35:01 +0000

    [New] Fix install sh

--- spacemacs_tmux_setup.sh
+++ spacemacs_tmux_setup.sh
@@ -1,9 +1,10 @@
-conda install -c conda-forge emacs
+# conda install -c conda-forge emacs
 ln -s ${PWD}/spacemacs ${HOME}/.spacemacs.d
 rm ${PWD}/spacemacs/init.el
 ln -s ${PWD}/spacemacs/.spacemacs.clean ${PWD}/spacemacs/init.el
 ln -s ${PWD}/tmux/.tmux.conf ${HOME}/.tmux.conf
-sudo ln -s ${PWD}/python/.config/flake8 ${HOME}/.config
-sudo ln -s ${PWD}/python/.config/yapf ${HOME}/.config
-sudo ln -s ${PWD}/python/.config/pylintrc ${HOME}/.config
+mkdir ${HOME}/.config
+ln -s ${PWD}/python/.config/flake8 ${HOME}/.config/flake8
+ln -s ${PWD}/python/.config/yapf ${HOME}/.config/yapf
+ln -s ${PWD}/python/.config/pylintrc ${HOME}/.config/pylintrc
 
