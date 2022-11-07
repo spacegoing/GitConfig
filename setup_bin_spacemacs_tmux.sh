061dee01d909b1304578e65a476ef92dc7ab0765
spacegoing <spacegoing@gmail.com>
2022-11-07 21:01:45 +0800

    [New] conda sources for china

--- setup_bin_spacemacs_tmux.sh
+++ setup_bin_spacemacs_tmux.sh
@@ -1,6 +1,7 @@
 # conda install -c conda-forge emacs
 ln -s ${PWD}/spacemacs ${HOME}/.spacemacs.d
 rm ${PWD}/spacemacs/init.el
+git update-index --skip-worktree ${PWD}/spacemacs/init.el
 ln -s ${PWD}/spacemacs/.spacemacs.clean ${PWD}/spacemacs/init.el
 ln -s ${PWD}/tmux/.tmux.conf ${HOME}/.tmux.conf
 mkdir ${HOME}/.config
@@ -22,3 +23,9 @@ ln -s ${PWD}/.bin/docker/drmrf ${HOME}/bin/docker/drmrf
 
 # manual setup
 ln -s ${PWD}/.bin/tmux/tsgs/tsg ${HOME}/bin/tmux/tsgs/tsg
+
+# python setup
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/free/ && \
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/ && \
+conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/ && \
+conda config --set show_channel_urls yes
