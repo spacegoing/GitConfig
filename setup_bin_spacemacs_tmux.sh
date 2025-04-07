82782598e38ff51b44d406bc355acacced182908
spacegoing <spacegoing@gmail.com>
2025-04-07 22:57:31 +0800

    Update setup_bin_spacemacs_tmux.sh

--- setup_bin_spacemacs_tmux.sh
+++ setup_bin_spacemacs_tmux.sh
@@ -30,3 +30,51 @@ conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/f
 conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/pkgs/main/ && \
 conda config --add channels https://mirrors.tuna.tsinghua.edu.cn/anaconda/cloud/conda-forge/ && \
 conda config --set show_channel_urls yes
+
+
+
+# >>> Configs starts from here <<<
+export LANG=en_US.UTF-8
+# >>> Configs ends here <<<
+
+# >>> my alias starts from here <<<
+alias di="docker images"
+alias dps="docker ps"
+alias dpsa="docker ps -a"
+drmc() { docker stop "$1" && docker rm -v "$1"; }
+alias drm="docker rm -f"
+alias drmiall="docker rmi $(docker images -f "dangling=true" -q)"
+alias drmall="docker rm -f $(docker ps -aq)"
+alias drmv="docker volume prune"
+dbash() { docker exec -it "$1" bash; }
+alias dcu="docker compose up -d"
+alias dcugpu="docker compose -f docker-compose.yml -f docker-compose.gpu.yml up -d"
+alias dcd="docker compose down"
+alias dcdrmv="docker compose down --volumes"
+alias dcps="docker compose ps"
+alias dclogs="docker compose logs"
+alias gitr="git pull origin master"
+tnew() { tmux new -s "$1"; }
+tat() { tmux a -t "$1"; }
+tkl() { tmux kill-session -t "$1"; }
+# >>> my alias ends here <<<
+
+# >>> my functions starts from here <<<
+dil() {
+    for image in $(docker images --format "{{.Repository}}:{{.Tag}}"); do
+        commit_label=$(docker inspect --format "{{ index .Config.Labels \"commit\" }}" $image)
+        if [ ! -z "$commit_label" ]; then
+            echo "$image: $commit_label"
+        fi
+    done
+}
+# >>> my functions ends here <<<
+
+############################################ BCE #################################
+function enableProxy(){
+  export http_proxy=http://10.16.0.38:18000
+  export https_proxy=http://10.16.0.38:18000
+}
+function disableProxy(){
+  unset http_proxy https_proxy
+}
