01d14510d7486a182d6dd34e369df54c9b82914c
spacegoing <spacegoing@gmail.com>
2025-06-02 23:24:13 +0800

    Update init_china.sh

--- init_china.sh
+++ init_china.sh
@@ -1,5 +1,46 @@
 #!/bin/bash
+# >>> Configs starts from here <<<
+export LANG=en_US.UTF-8
+# >>> Configs ends here <<<
 
+# >>> my alias starts from here <<<
+alias di="docker images"
+alias dps="docker ps"
+alias dpsa="docker ps -a"
+alias drm='docker rm -f'
+alias drmiall="docker rmi $(docker images -f "dangling=true" -q)"
+alias drmall="docker rm -f $(docker ps -aq)"
+alias drmv="docker volume prune"
+dbash() { docker exec -it "$1" bash; }
+alias dcu="docker compose up -d"
+alias dcugpu="docker compose -f docker-compose.yml -f docker-compose.gpu.yml up -d"
+alias dcd="docker compose down"
+alias dcdrmv="docker compose down --volumes"
+alias dcps="docker compose ps"
+alias dclogs="docker compose logs"
+alias gitr="git pull origin master"
+tnew() { tmux new -s "$1"; }
+tat() { tmux a -t "$1"; }
+tkl() { tmux kill-session -t "$1"; }
+# >>> my alias ends here <<<
+
+# >>> my functions starts from here <<<
+dil() {
+    for image in $(docker images --format "{{.Repository}}:{{.Tag}}"); do
+        commit_label=$(docker inspect --format "{{ index .Config.Labels \"commit\" }}" $image)
+        if [ ! -z "$commit_label" ]; then
+            echo "$image: $commit_label"
+        fi
+    done
+}
+# >>> my functions ends here <<<
+
+alias alin="docker login --username=63907871@qq.com registry.cn-hangzhou.aliyuncs.com"
+alias dkin="docker login --username=spacebnbk@gmail.com"
+
+
+
+# Old -----------------------------
 ## Github clone
 git config --global url."https://gitclone.com/".insteadOf https://
 
